7d27a902ae023ee2acea6e2f531e382d971b89aa
vorw <sultanalsanie23@gmail.com>
2025-06-03 17:04:44 +0000

    trying to clean the code

--- _printf.c
+++ _printf.c
@@ -7,16 +7,20 @@
 * @args: va_list containing the character
 * Return: number of characters printed
 */
+
 int print_char(va_list args)
 {
+
 char c = va_arg(args, int);
 return (write(1, &c, 1));
 }
+
 /**
 * print_string - prints a string
 * @args: va_list containing the string
 * Return: number of characters printed
 */
+
 int print_string(va_list args)
 {
 char *s = va_arg(args, char *);
@@ -26,12 +30,15 @@ s = "(null)";
 while (s[i])
 write(1, &s[i++], 1);
 return (i);
+
 }
+
 /**
 * _printf - simplified printf function
 * @format: format string
 * Return: number of characters printed
 */
+
 int _printf(const char *format, ...)
 {
 va_list args;
