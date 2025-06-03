f282609498f26503f8af97df75d9ccf319a0d755
moha88z <mohammed.alayda88@gmail.com>
2025-06-03 10:45:20 +0000

    gg

--- all.c
+++ all.c
@@ -1,1 +1,61 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 
+/**
+ * _printf - Simplified version of printf
+ * @format: format string
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+
+	if (!format)
+		return (-1);
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+				write(1, &c, 1);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(args, char *);
+				int i = 0;
+
+				if (!s)
+					s = "(null)";
+				while (s[i])
+				{
+					write(1, &s[i], 1);
+					count++;
+					i++;
+				}
+			}
+			else if (*format == '%')
+			{
+				write(1, "%", 1);
+				count++;
+			}
+		}
+		else
+		{
+			write(1, format, 1);
+			count++;
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (count);
+}
