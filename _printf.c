6bcd98210d0ca8c43d679119c4a386db0c9a71f2
moha88z <mohammed.alayda88@gmail.com>
2025-06-03 14:33:10 +0000

    gg

--- _printf.c
+++ _printf.c
@@ -1,15 +1,51 @@
+#include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
 
 /**
- * _printf - Custom printf function that handles %c, %s, and %%
- * @format: The format string
- * Return: Number of characters printed
+ * print_char - prints a character
+ * @args: argument list
+ * Return: number of characters printed
+ */
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	write(1, &c, 1);
+	return (1);
+}
+
+/**
+ * print_string - prints a string
+ * @args: argument list
+ * Return: number of characters printed
+ */
+int print_string(va_list args)
+{
+	char *s = va_arg(args, char *);
+	int i = 0;
+
+	if (!s)
+		s = "(null)";
+
+	while (s[i])
+	{
+		write(1, &s[i], 1);
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * _printf - custom printf function
+ * @format: format string
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int count = 0;
 	va_list args;
+	int count = 0;
 
 	if (!format)
 		return (-1);
@@ -21,37 +57,18 @@ int _printf(const char *format, ...)
 		if (*format == '%')
 		{
 			format++;
-			if (!*format) /* % at the end without specifier */
-			{
-				va_end(args);
+			if (!*format)
 				return (-1);
-			}
-			if (*format == 'c')
-			{
-				char c = va_arg(args, int);
-				write(1, &c, 1);
-				count++;
-			}
-			else if (*format == 's')
-			{
-				char *s = va_arg(args, char *);
-				int i = 0;
 
-				if (!s)
-					s = "(null)";
-				while (s[i])
-				{
-					write(1, &s[i], 1);
-					count++;
-					i++;
-				}
-			}
+			if (*format == 'c')
+				count += print_char(args);
+			else if (*format == 's')
+				count += print_string(args);
 			else if (*format == '%')
 			{
 				write(1, "%", 1);
 				count++;
 			}
-			/* if the specifier is not supported, do nothing */
 		}
 		else
 		{
