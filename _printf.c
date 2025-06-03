633cd37b98613a8116b3a54317fc1271771898d7
moha88z <mohammed.alayda88@gmail.com>
2025-06-03 10:43:43 +0000

    gg

--- _printf.c
+++ _printf.c
@@ -1,35 +1,59 @@
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
- * _printf - Custom printf function
- * @format: format string
- * Return: number of characters printed
+ * _printf - Simplified version of printf
+ * @format: The format string
+ * Return: Number of characters printed
  */
 int _printf(const char *format, ...)
 {
+	int count = 0;
 	va_list args;
-	int i = 0, count = 0;
 
 	if (!format)
 		return (-1);
 
 	va_start(args, format);
 
-	while (format[i])
+	while (*format)
 	{
-		if (format[i] == '%')
+		if (*format == '%')
 		{
-			i++;
-			if (!format[i])
-				return (-1);
-			count += handle_specifier(format[i], args);
+			format++;
+			if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+				write(1, &c, 1);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(args, char *);
+				int i = 0;
+
+				if (!s)
+					s = "(null)";
+				while (s[i])
+				{
+					write(1, &s[i], 1);
+					count++;
+					i++;
+				}
+			}
+			else if (*format == '%')
+			{
+				write(1, "%", 1);
+				count++;
+			}
 		}
 		else
 		{
-			write(1, &format[i], 1);
+			write(1, format, 1);
 			count++;
 		}
-		i++;
+		format++;
 	}
 
 	va_end(args);
