4b59c8a1d3d0c49591f13ff8e01ed43e29a15c4d
sarunh10 <sarunh10@gmail.com>
2025-06-08 23:37:08 +0000

    task holbertonschool-printf

--- _printf.c
+++ _printf.c
@@ -108,8 +108,6 @@ int _printf(const char *format, ...)
 				count += write(1, "%", 1);
 			else if (*format == 'd' || *format == 'i')
 				count += print_int(args);
-			else if (*format == 'b')
-				count += print_binary(va_arg(args, unsigned int));
 
 			else
 			{
